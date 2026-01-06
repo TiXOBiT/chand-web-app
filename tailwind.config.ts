7ed8c0f5e20d79827043144d157df112642e820f
Mohammadreza Jafari <86445862+TiXOBiT@users.noreply.github.com>
2026-01-06 21:45:45 +0330

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,8 @@
-content: [
-  "./app/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-]
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "class",
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: { extend: {} },
+  plugins: [],
+} satisfies Config;
